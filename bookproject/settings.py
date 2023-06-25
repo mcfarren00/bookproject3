eb8c45e1f97f57ca0f0cab14e3b0c5a2e7e27e6a
mcfarren00 <kota-627-owl@outlook.jp>
2023-06-25 22:39:00 +0900

    cloudinary6

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -134,10 +134,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR/ "static"
-DEFAULT_FILES_STORAGE="cloudinary_storage.strage.MediaCloudinaryStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL="/media/"
-MEDIA_ROOT=BASE_DIR/"media"
+DEFAULT_FILES_STORAGE="cloudinary_storage.strage.MediaCloudinaryStorage"
+# MEDIA_ROOT=BASE_DIR/"media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
