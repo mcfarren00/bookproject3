92f5a20fe04c713640c4d3bb6589a0ca995311e8
mcfarren00 <kota-627-owl@outlook.jp>
2023-06-25 20:40:54 +0900

    makesuperuser6

--- bookproject/urls.py
+++ bookproject/urls.py
@@ -18,13 +18,11 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/",include("accounts.urls")),
     path("",include("book.urls")),
-    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
