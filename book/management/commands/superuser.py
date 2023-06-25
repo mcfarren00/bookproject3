cf254b5286ed39db7bca53c6335bddc88e19e65c
mcfarren00 <kota-627-owl@outlook.jp>
2023-06-25 20:23:49 +0900

    makesuperuser4

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-from . import settings
+from django.conf import settings
 
 User = get_user_model()
 
